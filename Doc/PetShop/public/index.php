312867a817fdc0abe05868dc80a2b219c5ed0fe0
LZs-RFT <43699089+LZs-RFT@users.noreply.github.com>
2018-11-14 22:11:06 +0100

    keresés gomb
    A gomb átnavigál a keresés oldalra + a szükséges html körítés

--- Doc/PetShop/public/index.php
+++ Doc/PetShop/public/index.php
@@ -3,3 +3,13 @@ require("../resources/config.php");
 
 include_once(TEMPLATE_FRONT . "header.php");
 ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+         <a href="http://localhost/Doc/PetShop/search/index.php">Keresés</a>
+    </body>
+</html>
