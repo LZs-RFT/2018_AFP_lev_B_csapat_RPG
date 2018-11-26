1554f8d76f970b6e5f0d1840c456f6e842c02b00
LZs-RFT <43699089+LZs-RFT@users.noreply.github.com>
2018-11-26 17:31:46 +0100

    adatbázis kezdemény a kereséshez

---
+++ Doc/PetShop/core/config.php
@@ -0,0 +1,7 @@
+<?php
+ // A működéshez szükséges konstansok def.
+    define('DB_TYPE', 'mysql');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'petshop');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
