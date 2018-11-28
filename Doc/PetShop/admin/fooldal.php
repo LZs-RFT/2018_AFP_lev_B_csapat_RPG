4611747f9caa4da7f1b42663a29fa08781a22e07
Nagy Annamária <annamaria.nagy06@gmail.com>
2018-11-28 21:53:04 +0100

    Kutyák kilistázása elkészült

--- Doc/PetShop/admin/fooldal.php
+++ Doc/PetShop/admin/fooldal.php
@@ -1,5 +1,5 @@
 <?php
-	//include_once '../core/database.php';
+	include_once '../core/database.php';
 ?>
 <!DOCTYPE html>
 <html>
--- Doc/PetShop/admin/list.php
+++ Doc/PetShop/admin/list.php
@@ -1,1 +1,30 @@
-<p>Kutyák itt lesznek kilistázva</p>
+<?php
+	$query = "SELECT * FROM kutya";
+	$kutyak = QL_array($query);
+?>
+<table>
+	<!-- Fejléc -->
+	<tr>
+		<th>Kép</th>
+		<th>Név</th>
+		<th>Fajta</th>
+		<th>Szőrhossz</th>
+		<th>Ugatás</th>
+		<th>Méret</th>
+		<th></th>
+		<th></th>
+	</tr>
+	<!-- Adatok kilistázása az adatbázisból -->
+<?php foreach($kutyak as $x) { ?>
+	<tr>
+		<td><img src="<?php echo $x['kep'] ; ?>" /></td>
+		<td><?php echo $x['name'] ; ?></td>
+		<td><?php echo $x['fajta'] ; ?></td>
+		<td><?php echo $x['szorhossz'] ; ?></td>
+		<td><?php echo $x['ugatas'] ; ?></td>
+		<td><?php echo $x['meret'] ; ?></td>
+		<td><a href="">Szerkesztés</a></td>
+		<td><a href="">Törlés</a></td>
+	</tr>
+<?php } ?>
+</table>
