d9bf5205c36437584c8c9257185b9deade8d1b7b
kpaprika <klaudia.paprika@finastra.com>
2018-12-07 00:55:44 +0100

    Bug fix and minor changes of UI

--- Doc/PetShop/resources/config.php
+++ Doc/PetShop/resources/config.php
@@ -10,5 +10,4 @@ defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "/templates/front/");
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "/templates/back/");
 defined("SEARCH") ? null : define("SEARCH", __DIR__ . "../".DS ."search/");
-define("ROOT") ? null : define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 ?>
--- Doc/PetShop/resources/templates/back/content.php
+++ Doc/PetShop/resources/templates/back/content.php
@@ -4,7 +4,11 @@
     <!-- content body -->
     <section id="slider"><a href="#"><img src="https://www.thinkinsurance.co.uk/uploads/archive/content_pets-fireworks-yellow_861388.jpg" alt="SEMMI"></a></section>
     <section id="shout">
-      <p>Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed. Facilispede estibulum nulla orna nisl velit elit ac aliquat non tincidunt namjusto cras urna.</p>
+      <p>“
+
+Animals are not property or "things" but rather living organisms, subjects of a life, who are worthy of our compassion, respect, friendship, and support.
+
+Marc Bekoff</p>
     </section>
 
     <?php
@@ -16,18 +20,18 @@
       <h1>Services We Offer</h1>
       <section id="services" class="clear">
         <article>
-          <figure><img src="images/demo/32x32.gif" width="32" height="32" alt=""></figure>
-          <strong>Lorum ipsum dolor</strong>
+          <figure><img src="https://cdn0.iconfinder.com/data/icons/fatcow/32x32/cat.png" width="32" height="32" alt=""></figure>
+          <strong>To be a good Cat owner you should ...</strong>
           <p class="more"><a href="#">Read More &raquo;</a></p>
         </article>
         <article>
-          <figure><img src="../../images/.gif" width="32" height="32" alt=""></figure>
+          <figure><img src="https://cdn3.iconfinder.com/data/icons/fatcow/32/heart.png" width="32" height="32" alt=""></figure>
           <strong>"If having a soul means being able to feel love and loyalty and gratitude, then animals are better off than a lot of humans."―James Herriot</p>
           <p class="more"><a href="#">Read More &raquo;</a></p>
         </article>
         <article class="last">
-          <figure><img src="images/demo/32x32.gif" width="32" height="32" alt=""></figure>
-          <strong>Lorum ipsum dolor</strong>
+          <figure><img src="https://dl1.cbsistatic.com/i/r/2017/11/12/e16a1763-5dbb-4dbc-858a-075c5c54333d/thumbnail/32x32/c0cf9901b5ef5438d9e82b8a8334a288/iconimg11063.png" width="32" height="32" alt=""></figure>
+          <strong>To be a good Dog owner you should ...<</strong>
           <p class="more"><a href="#">Read More &raquo;</a></p>
         </article>
       </section>
